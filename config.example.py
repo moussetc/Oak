365e623e4b69a1503f08a1d1be42c49d1bfc9f79
wormsy <wormsy@protonmail.com>
2018-11-05 10:39:21 +0100

    Adding example config

---
+++ config.example.py
@@ -0,0 +1,22 @@
+#####DATABASE, limited to MySQL and MariaDB#####
+host = 'localhost'  #host
+user = 'root'  #database user
+password = 'root'  #database password
+database = 'monocle'  #database name
+
+language = 'FR'
+
+# Those are fake, don't use them
+roles_sectors = {
+    502697614155392689, # Sector 1
+    502406049664304610, # Sector 2
+}
+
+raid_ex_channels = {
+    'raid-egg',
+    'raid-spam',
+    'raid-bacon',
+}
+
+raid_channel = 504307932324764852
+quest_channel = 445222271314785492 
